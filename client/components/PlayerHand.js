13ac1f000132616ef07adf4ca4dda2108bb56206
Cody Jensen <jensen.cody.e@gmail.com>
2019-01-22 11:24:07 -0600

    push up for merge to master

--- client/components/PlayerHand.js
+++ client/components/PlayerHand.js
@@ -22,6 +22,7 @@ class PlayerHand extends React.Component {
   }
 
   render() {
+    console.log(this.props.me.hand)
     return (
       <div className="player-hand">
         {this.props.me.hand.map(card => {
