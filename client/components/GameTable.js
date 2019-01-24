a4ec4e5f4739013e99de1dcdd7a0fc3fdf384be0
neilscheuermann <42921947+neilscheuermann@users.noreply.github.com>
2019-01-24 10:20:09 -0700

    Initial view set-up (#3)

---
+++ client/components/GameTable.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class GameTable extends Component {
+  render() {
+    return (
+      <div>Insert nested components here: PlayerArea GameBoard Hand Card</div>
+    )
+  }
+}
+
+export default GameTable
