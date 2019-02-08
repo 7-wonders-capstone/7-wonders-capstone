76df2af90a89e3fd815ef6180017c5e8e06171f1
Jeremy Henrard <jhenrard@gmail.com>
2019-02-08 17:57:59 -0500

    bugfix: play card button in trade modal

--- client/components/TradeModal.js
+++ client/components/TradeModal.js
@@ -6,6 +6,7 @@ import {compose} from 'redux'
 import {connect} from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
 import {selectAction} from '../store/selectedAction'
+import playCard from '../store/playCard'
 
 class TradeModal extends Component {
   constructor() {
@@ -131,6 +132,7 @@ class TradeModal extends Component {
                   this.close()
                   this.props.preparePlay(this.props.me.email)
                   this.props.selectAction('play')
+                  this.props.playCard(selectedCard)
                 }}
                 positive
               >
@@ -166,7 +168,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    selectAction: action => dispatch(selectAction(action))
+    selectAction: action => dispatch(selectAction(action)),
+    playCard: card => dispatch(playCard(card))
   }
 }
 
